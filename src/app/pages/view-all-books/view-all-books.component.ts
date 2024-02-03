8bf8c71f685840830e65ad2a53e88224039fc146
Minura Dinupriya <mranaweera793@gmail.com>
2024-02-03 17:25:45 +0530

    front working fine with delete api

---
+++ src/app/pages/view-all-books/view-all-books.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-view-all-books',
+  standalone: true,
+  imports: [HttpClientModule, FormsModule, CommonModule],
+  templateUrl: './view-all-books.component.html',
+  styleUrl: './view-all-books.component.css',
+})
+export class ViewAllBooksComponent implements OnInit {
+  private http;
+  public bookList: any = {};
+  public selectedBook: any;
+
+  constructor(private httpCliant: HttpClient) {
+    this.http = httpCliant;
+  }
+
+  ngOnInit(): void {
+    this.loadBooks();
+  }
+
+  loadBooks() {
+    this.http.get('http://localhost:8080/book/get').subscribe((data) => {
+      this.bookList = data;
+      console.log(this.bookList);
+    });
+  }
+
+  deleteBook() {
+    let api = 'http://localhost:8080/book/' + this.selectedBook.id;
+    this.http
+      .delete(api, { responseType: 'text' })
+      .subscribe((responce: string) => {
+        console.log(responce);
+        this.loadBooks();
+        this.selectedBook = null;
+      });
+  }
+
+  setSelectedBook(book: any) {
+    this.selectedBook = book;
+    console.log('Set Selected Book' + book.id);
+  }
+}
