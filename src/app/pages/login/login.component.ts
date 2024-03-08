648152f29245d145e93b812fbc189cb3887c0f03
Minura Dinupriya <mranaweera793@gmail.com>
2024-03-08 10:44:13 +0530

    user successfully navigate to view borrowers after correct login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,HttpClientModule,FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  constructor(private http:HttpClient,private router:Router){
 
+  }
+  loginObj:any={
+    "userName":"",
+    "password":""
+  }
+
+  logIn(){
+    this.http.post("http://localhost:8080/validate-login",this.loginObj).subscribe((respone:any)=>{
+      console.log(respone)
+      if(respone){
+        this.router.navigate(['app-view-borrowers'])
+      }else{
+        alert("you can not login!!")
+      }
+    })
+  }
 }
