ea959782728b0b5edb9c1641c942a7d7da962448
Minura Dinupriya <mranaweera793@gmail.com>
2024-03-26 09:39:55 +0530

    add timer to log out

--- src/app/common/nav/nav.component.ts
+++ src/app/common/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-nav',
@@ -9,5 +10,36 @@ import { RouterLink } from '@angular/router';
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
+  constructor(private router:Router) {
+    
+  }
+  logOut() {
+    let timerInterval: any;
+    Swal.fire({
+      title: "Auto close alert!",
+      html: "I will close in <b></b> milliseconds.",
+      timer: 2000,
+      timerProgressBar: true,
+      didOpen: () => {
+        Swal.showLoading();
+        const timer = Swal.getPopup()?.querySelector("b"); // Optional chaining
+        if (timer) {
+          timerInterval = setInterval(() => {
+            timer.textContent = `${Swal.getTimerLeft()}`;
+          }, 100);
+        }
+      },
+      willClose: () => {
+        clearInterval(timerInterval);
+      }
+    }).then((result) => {
+      if (result.dismiss === Swal.DismissReason.timer) {
+        console.log("I was closed by the timer");
+        this.router.navigate(["login"]);
+      }
+    });
+    
+  }
+  
 
 }
